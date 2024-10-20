71dcfe7cf06b6efc60b3a17af54c559a7460bf27
perestr3lo <pallestrelli@pm.me>
2024-10-19 22:18:46 -0300

    Bugfix: ItemPedidoRequestUpdateDto (Internal Error 500)

--- src/main/java/br/com/fiap/ecommerce/dtos/ItemPedidoRequestUpdateDto.java
+++ src/main/java/br/com/fiap/ecommerce/dtos/ItemPedidoRequestUpdateDto.java
@@ -2,23 +2,47 @@ package br.com.fiap.ecommerce.dtos;
 
 import java.math.BigDecimal;
 
+import br.com.fiap.ecommerce.model.Pedido;
+import br.com.fiap.ecommerce.model.Produto;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 
 import br.com.fiap.ecommerce.model.ItemPedido;
+import org.modelmapper.TypeMap;
+import org.modelmapper.convention.MatchingStrategies;
 
 
 @Data
 public class ItemPedidoRequestUpdateDto {
-	private Long idPedido;
+    private Long idPedido;
     private Long idProduto;
     private BigDecimal quantidade;
     private BigDecimal valorTotal;
     private static final ModelMapper modelMapper = new ModelMapper();
-	public ItemPedido toModel(Long id) {
-		ItemPedido result = modelMapper.map(this, ItemPedido.class);
-        result.setId(id);
-        return result;
-    }  
-    
+
+    static {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        TypeMap<ItemPedidoRequestUpdateDto, ItemPedido> typeMap = modelMapper.createTypeMap(ItemPedidoRequestUpdateDto.class, ItemPedido.class);
+
+        typeMap.addMappings(mapper -> {
+            mapper.map(src -> {
+                Produto produto = new Produto();
+                produto.setId(src.getIdProduto());
+                return produto;
+            }, ItemPedido::setIdProduto);
+            mapper.map(src -> {
+                Pedido pedido = new Pedido();
+                pedido.setId(src.getIdPedido());
+                return pedido;
+            }, ItemPedido::setIdPedido);
+        });
+    }
+
+    public ItemPedido toModel(Long id) {
+        ItemPedido itemPedido = modelMapper.map(this, ItemPedido.class);
+        itemPedido.setId(id);
+        return itemPedido;
+    }
 }
+    
+
